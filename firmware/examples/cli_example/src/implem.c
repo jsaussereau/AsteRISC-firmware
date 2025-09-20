78ccb243055db313b355be6089ac5246167b357e
Jonathan Saussereau <jsaussereau@protonmail.com>
2025-09-20 10:32:47 +0200

    Update copyright notice

--- firmware/examples/cli_example/src/implem.c
+++ firmware/examples/cli_example/src/implem.c
@@ -1,13 +1,24 @@
-/*
- * Copyright(C) 2022 by Jonathan Saussereau. All rights reserved.
- * 
- * All source codes and documentation contain proprietary confidential
- * information and are distributed under license. It may be used, copied
- * and/or disclosed only pursuant to the terms of a valid license agreement
- * with Jonathan Saussereau. This copyright must be retained at all times.
- * 
- * implem.c
- */
+/**********************************************************************\
+*                               AsteRISC                               *
+************************************************************************
+*
+* Copyright (C) 2022 Jonathan Saussereau
+*
+* This file is part of AsteRISC.
+* AsteRISC is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* AsteRISC is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with AsteRISC. If not, see <https://www.gnu.org/licenses/>.
+*
+*/
 
 #include "implem.h" 
 
